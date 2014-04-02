107abaf85d93db4f78130be55946691cde75834f
Daniel Jones <tortxof@gmail.com>
2014-04-02 15:42:52 -0400

    Added roast ramping routine.

--- roaster_controller/roaster_controller.ino
+++ roaster_controller/roaster_controller.ino
@@ -1,10 +1,45 @@
+const int FAN_PIN = 11;
+const int HEAT_PIN = 13;
+
+unsigned long dry_delay = 4 * 60 * 1000; // 4 minutes in milliseconds.
+unsigned long cool_delay = 2 * 60 * 1000;
+unsigned long roast_interval = 30 * 1000;
+int fan_dry = 200;
+int fan_start = 160;
+int fan_end = 100;
+int fan_cool = 120;
+// 12 is fast. 20 is medium. 30 is slow.
+int intervals = 20;
+int fan_step = (fan_start - fan_end) / intervals;
+
 int heat = 0;
 int fan = 0;
 
+void doRoast() {
+  // Spool up fan
+  for (int i = 0; i <= fan_dry; i++) {
+    analogWrite(FAN_PIN, i);
+    delay(10);
+  }
+  // Turn on heat and wait for drying period.
+  digitalWrite(HEAT_PIN, HIGH);
+  delay(dry_delay);
+  // Ramp down fan speed over time
+  for (int i = fan_start; i >= fan_end; i -= fan_step) {
+    analogWrite(FAN_PIN, i);
+    delay(roast_interval); // delay 30 seconds.
+  }
+  // Cooling period
+  digitalWrite(HEAT_PIN, LOW);
+  analogWrite(FAN_PIN, fan_cool);
+  delay(cool_delay); // Cool for 2 minutes.
+  analogWrite(FAN_PIN, 0);
+}
+
 void setup() {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(HEAT_PIN, OUTPUT);
+  pinMode(FAN_PIN, OUTPUT);
 }
 
 void loop() {
